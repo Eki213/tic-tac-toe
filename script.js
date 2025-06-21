8b5abb54f36d685cb14d6142d811cab48bf351b2
Ekio <141391736+Eki213@users.noreply.github.com>
2025-06-21 00:09:42 -0300

    Implement DOM and Console controllers
    Both modules share the same game module as the core logic layer,
    separating game state from rendering logic.

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const createPlayer = function(name, mark) {
     const getName = () => name;
+    const setName = (newName) => name = newName;
     const getMark = () => mark;
 
-    return {getName, getMark};
+    return { getName, getMark, setName };
 };
 
 const gameboard = (function() {
     const EMPTY_CELL = " ";
-    const gameboard = Array(9).fill(EMPTY_CELL, 0);
+    const gameboard = Array(9).fill(EMPTY_CELL);
     const WINNING_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
                             [0, 3, 6], [1, 4, 7], [2, 5, 8],
                             [0, 4, 8], [2, 4, 6]];
@@ -25,13 +26,17 @@ const gameboard = (function() {
     
     const isFilled = () => gameboard.every((mark) => mark !== EMPTY_CELL);
 
-    const checkWinner = function() {
+    const checkResult = function() {
         const winningCombination = WINNING_COMBINATIONS.find((combination) => isThreeInARow(...combination));
-        let winner = null;
+        let result = null;
 
-        if (winningCombination) winner = getMark(winningCombination[0]);
+        if (winningCombination) {
+            result = getMark(winningCombination[0]);
+        } else if (isFilled()) {
+            result = "Tie";
+        }
 
-        return winner;
+        return result;
     };
 
     const printBoard = () => console.log(` ${getMark(0)} | ${getMark(1)} | ${getMark(2)}
@@ -44,7 +49,7 @@ const gameboard = (function() {
 
     const clearBoard = () => gameboard.fill(EMPTY_CELL, 0);
 
-    return { markBoard, checkWinner, isFilled, printBoard, isLegal, clearBoard };
+    return { markBoard, checkResult, printBoard, isLegal, clearBoard, getMark };
 })();
 
 const game = (function () {
@@ -52,54 +57,144 @@ const game = (function () {
     const nextTurn = () => turns++;
     const players = [createPlayer("Player1", "X"), createPlayer("Player2", "O")];
     const getCurrentPlayer = () => players[turns % 2];
-    let winnerMark;
-    let isTie;
+    let winnerMark = null;
 
-    const endGame = function() { 
-        winnerMark = gameboard.checkWinner();
-        isTie = gameboard.isFilled();
+    const endGame = () => winnerMark !== null;
 
-        return winnerMark !== null || isTie;
-    };
-  
-    const getSelectedCell = function() {
-        while (true) {
-            const cell = prompt("Cell number (0-8)");
-            if (gameboard.isLegal(cell)) return cell;
-        }
-    }
-
-    const playTurn = function () {
-        const currentPlayer = getCurrentPlayer();
-        const mark = currentPlayer.getMark();
-        const cell = getSelectedCell();
+    const playTurn = function (cell) {
+        const mark = getCurrentPlayer().getMark();
         gameboard.markBoard(cell, mark);
+        winnerMark = gameboard.checkResult();
+        nextTurn();
     };
     
     const getWinner = () => players.find((player) => player.getMark() === winnerMark);
 
+    const resetGame = function() {
+        gameboard.clearBoard();
+        winnerMark = null;
+        turns = 0;
+    }
+
+    const changePlayerNames = function(names) {
+        names.forEach((name, index) => { if (name) players[index].setName(name) });
+    };
+
+    return { playTurn, resetGame, endGame, getWinner, changePlayerNames, getCurrentPlayer };
+})();
+
+const displayController = (function() {
+    const gameContainer = document.querySelector(".game");
+    const gridContainer = document.querySelector(".board");
+    const resultsContainer = document.querySelector(".results");
+    const introScreen = document.querySelector(".intro-screen");
+    const changeNamesButton = document.querySelector("button[type='submit']");
+    const gameButton = document.querySelector(".game-button");
+    let gameStarted = false;
+
+    const createGrid = function() {
+        for (let i = 0; i < 9; i++) {
+            const cell = document.createElement("button");
+            cell.className = "cell";
+            cell.setAttribute("type", "button");
+            cell.dataset.index = i;
+            gridContainer.appendChild(cell);
+        }
+    };
+
+    const updateGrid = function() {
+        const cells = document.querySelectorAll(".cell");
+        cells.forEach(updateCell);
+    };
+
+    const updateTurnAnnouncement = function() {
+        const currentPlayer = game.getCurrentPlayer();
+        resultsContainer.textContent = `${currentPlayer.getName()}'s turn (${currentPlayer.getMark()})`;
+    };
+
+    const updateCell = (cell) => cell.textContent = gameboard.getMark(cell.dataset.index);
+
+    const displayResults = function() {
+        const winner = game.getWinner();
+        if (winner) {
+            resultsContainer.textContent = `The winner is ${winner.getName()}!`
+        } else {
+            resultsContainer.textContent = "It's a tie!";
+        }
+    };
+
+    const clickHandlerBoard = function (event) {
+        const cell = event.target;
+        const index = cell.dataset.index;
+        if (!index || !gameboard.isLegal(index) || game.endGame() || !gameStarted) return;
+
+        game.playTurn(index);
+        updateCell(cell);
+        updateTurnAnnouncement();
+
+        if (game.endGame()) displayResults();
+    };
+
+    gridContainer.addEventListener("click", clickHandlerBoard);
+
+    const gameButtonHandler = function() {
+        if (!gameStarted) {
+            gameButton.textContent = "Play Again";
+            gameStarted = true;
+        } else {
+            game.resetGame();
+            updateGrid();
+        }
+
+        updateTurnAnnouncement();
+    };
+
+    const namesFormHandler = function(event) {
+        event.preventDefault();
+        const inputs = document.querySelectorAll(".intro-screen input[type='text']");
+        const names = [...inputs].map((input) => input.value);
+        game.changePlayerNames(names);
+        
+        introScreen.classList.add("hidden");
+        gameContainer.classList.remove("hidden");
+    };
+
+    changeNamesButton.addEventListener("click", namesFormHandler);
+    gameButton.addEventListener("click", gameButtonHandler);
+    createGrid();
+})();
+
+const consoleController = (function() {
+    const init = function() {
+        while (!game.endGame()) {
+            announceTurn();
+            game.playTurn(getSelectedCell());
+            gameboard.printBoard();
+        }
+        announceResult();
+        game.resetGame();
+    };
+
+    const announceTurn = function() {
+        const currentPlayer = game.getCurrentPlayer();
+        console.log (`${currentPlayer.getName()}'s turn (${currentPlayer.getMark()})`);
+    };
+
     const announceResult = function() {
-        if (!isTie) {
-            const winner = getWinner();
-            console.log(`The winner is ${winner.getName()} with mark ${winnerMark}.`);
+        const winner = game.getWinner();
+        if (winner) {
+            console.log(`The winner is ${winner.getName()} with mark ${winner.getMark()}.`);
         } else {
             console.log("It's a tie.");
         }
     };
 
-    const resetGame = function() {
-        gameboard.clearBoard();
-        turns = 0;
+    const getSelectedCell = function() {
+        while (true) {
+            const cell = prompt("Enter Cell Number (0-8):");
+            if (gameboard.isLegal(cell)) return cell;
+        }
     }
 
-    const init = function() {
-        while (!endGame()) {
-            playTurn();
-            gameboard.printBoard();
-            nextTurn();
-        }
-        announceResult();
-        resetGame();
-    };
     return { init };
 })();
